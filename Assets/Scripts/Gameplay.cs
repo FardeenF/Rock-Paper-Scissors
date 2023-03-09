d04196e04dcdfe9641caf9508948e77f5cc35115
Daye Fubara <d.d.fubara@gmail.com>
2023-03-08 19:57:09 -0500

    Added Sounds (I guess?)

--- Assets/Scripts/Gameplay.cs
+++ Assets/Scripts/Gameplay.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 using TMPro;
 
 public class Gameplay : MonoBehaviour
@@ -12,6 +14,17 @@ public class Gameplay : MonoBehaviour
     public GameObject PlayerPaper;
     public GameObject PlayerScissors;
 
+    public AudioSource audioSource;
+
+    public AudioClip clickSound;
+    public AudioClip winSound;
+    public AudioClip loseSound;
+    public AudioClip tieSound;
+
+    public Button winButton;
+    public Button loseButton;
+    public Button tieButton;
+
     private string playerCurrent = "paper";
     private string aiCurrent = "paper";
 
@@ -20,6 +33,9 @@ public class Gameplay : MonoBehaviour
     public int aiChoice;
 
     private bool gameStarted = false;
+    private bool hasWon = false;
+    private bool hasLost = false;
+    private bool hasDrawn = false; 
     
 
     // Start is called before the first frame update
@@ -37,14 +53,17 @@ public class Gameplay : MonoBehaviour
             if (PlayerRock.activeInHierarchy == true && AIRock.activeInHierarchy == true)
             {
                 mainText.text = "Tied! Play again?";
+                hasDrawn = true;
             }
             else if (PlayerPaper.activeInHierarchy == true && AIPaper.activeInHierarchy == true)
             {
                 mainText.text = "Tied! Play again?";
+                hasDrawn = true;
             }
             else if (PlayerScissors.activeInHierarchy == true && AIScissors.activeInHierarchy == true)
             {
                 mainText.text = "Tied! Play again?";
+                hasDrawn = true;
             }
             else
             {
@@ -56,14 +75,17 @@ public class Gameplay : MonoBehaviour
             if (PlayerRock.activeInHierarchy == true && AIScissors.activeInHierarchy == true)
             {
                 mainText.text = "You Win! Play Again?";
+                hasWon = true;
             }
             else if (PlayerPaper.activeInHierarchy == true && AIRock.activeInHierarchy == true)
             {
                 mainText.text = "You Win! Play Again?";
+                hasWon = true;
             }
             else if (PlayerScissors.activeInHierarchy == true && AIPaper.activeInHierarchy == true)
             {
                 mainText.text = "You Win! Play Again?";
+                hasWon = true;
             }
             else
             {
@@ -75,18 +97,40 @@ public class Gameplay : MonoBehaviour
             if (AIRock.activeInHierarchy == true && PlayerScissors.activeInHierarchy == true)
             {
                 mainText.text = "You Lose! Play Again?";
+                hasLost = true;
             }
             else if (AIPaper.activeInHierarchy == true && PlayerRock.activeInHierarchy == true)
             {
                 mainText.text = "You Lose! Play Again?";
+                hasLost = true;
             }
             else if (AIScissors.activeInHierarchy == true && PlayerPaper.activeInHierarchy == true)
             {
                 mainText.text = "You Lose! Play Again?";
+                hasLost = true;
             }
             else
             {
                 Debug.Log("Something happened we did not expect!!!!!!");
+            }
+
+            //handle sounds seperately based on bool conditions 
+            if (hasWon == true)
+            {
+                SimulateClickWin();
+                hasWon = false;
+            }
+
+            if (hasLost == true)
+            {
+                SimulateClickWin();
+                hasLost = false;
+            }
+
+            if (hasDrawn == true)
+            {
+                SimulateClickWin();
+                hasDrawn = false;
             }
         }
     }    
@@ -149,4 +193,49 @@ public class Gameplay : MonoBehaviour
             AIScissors.SetActive(true);
         }
     }
+
+    public void playClick()
+    {
+        audioSource.PlayOneShot(clickSound, 1.0F);
+    }
+
+    public void playWin()
+    {
+        audioSource.PlayOneShot(winSound, 1.0F);
+    }
+
+    public void playLose()
+    {
+        audioSource.PlayOneShot(loseSound, 1.0F);
+    }
+
+    public void playTie()
+    {
+        audioSource.PlayOneShot(tieSound, 1.0F);
+    }
+
+    private void SimulateClickWin()
+    {
+        if (winButton != null)
+        {
+            ExecuteEvents.Execute(winButton.gameObject, new PointerEventData(EventSystem.current), ExecuteEvents.pointerClickHandler);
+        }
+    }
+
+    private void SimulateClickLoss()
+    {
+        if (loseButton != null)
+        {
+            ExecuteEvents.Execute(loseButton.gameObject, new PointerEventData(EventSystem.current), ExecuteEvents.pointerClickHandler);
+        }
+    }
+
+    private void SimulateClickTie()
+    {
+        if (tieButton != null)
+        {
+            ExecuteEvents.Execute(tieButton.gameObject, new PointerEventData(EventSystem.current), ExecuteEvents.pointerClickHandler);
+        }
+    }
+
 }
